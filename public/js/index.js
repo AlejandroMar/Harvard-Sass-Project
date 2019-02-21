1a2e81ac4e28ac2e9aa2943f69c7b7da94e120c0
Alejo <motungo@hotmail.com>
2019-02-21 08:43:21 +0100

    add menu.html and create menu component using bootstrap tabs

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,4 @@
-function initMap() {
-            // The location of medellin
-            var medellin = { lat: 6.157232, lng: -75.590853 };
-            // The map, centered at medellin
-            var map = new google.maps.Map(
-                document.getElementById('map'), { zoom: 12, center: medellin });
-            // The marker, positioned at medellin
-            var marker = new google.maps.Marker({ position: medellin, map: map });
-}
+
 
 // A $( document ).ready() block.
 $( document ).ready(function() {
