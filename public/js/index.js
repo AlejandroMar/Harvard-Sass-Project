7ba9c9c99ecf66f268934bb679e02f559019cc8b
Alejo <motungo@hotmail.com>
2019-02-26 12:23:58 +0100

    change navbar background color on scroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,14 @@
-
-
-// A $( document ).ready() block.
-$( document ).ready(function() {
-    
+$(document).ready(function() {
+  // change navbar background color on scroll
+  $(document).scroll(function() {
+    // get the height of the hero image in the header
+    const headerHeight = $(".header").height();
+    const scroll = $(window).scrollTop();
+    if (scroll > headerHeight) {
+      // see the classes in _custom-nav.scss
+      $(".navigation").addClass("scrolled");
+    } else {
+      $(".navigation").removeClass("scrolled");
+    }
+  });
 });
