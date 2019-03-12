8ea93a6683065256da9d094a67e1b280c8e6e826
Alejo <motungo@hotmail.com>
2019-03-12 16:32:51 +0100

    add bg color to menu on click

--- public/js/index.js
+++ public/js/index.js
@@ -11,4 +11,7 @@ $(document).ready(function() {
       $(".navigation").removeClass("scrolled");
     }
   });
+  $(".navbar").click(function() {
+    $(this).toggleClass("navbar-clicked");
+  });
 });
